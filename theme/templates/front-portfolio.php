0461c71fc1915115e44c44442d8b2249fdeea0c2
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2024-02-29 16:07:02 -0500

    Add portafolio and services

--- theme/templates/front-portfolio.php
+++ theme/templates/front-portfolio.php
@@ -17,57 +17,79 @@
 get_header();
 
 // Banner
-
+ get_template_part('template-parts/banners/internal', 'page')
 ?>
-<section class="w-full h-[50rem] bg-cover bg-center flex justify-start items-center" >
-	<?= get_template_part('template-parts/banners/internal', 'page') ?>
+<section class="container">
+<!-- Testimoniales -->
+<section class="w-full h-[50rem] bg-auto bg-white  flex items-start justify-center">
+	<div class="container ">
+		<h2 class="text-[40px] text-sky-blue font-black  text-center"> TESTIMONIALES</h2>
+
+		<div class=" container flex justify-strech gap-40">
+
+			<div class="slider grid grid-cols-1 grid-rows-1 gap-5 overflow-hidden relative">
+				<div class="slides grid grid-cols-3 gap-5 transition-transform duration-300">
+					<div class="slide flex flex-col col-span-2 row-span-3 justify-center  items-center">
+						<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244253804_1913345092171178_377135258163533003_n.jpeg" alt="">
+					</div>
+					<div class="slide flex flex-col justify-center  items-center">
+						<img class ="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244250619_1913344878837866_7039778878385101219_n.jpeg" alt="">
+					</div>
+					<div class="slide flex flex-col justify-center  items-center">
+						<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244273434_1913344755504545_8294063936522850181_n.jpeg" alt="">
+					</div>
+					<div class="slide flex flex-col justify-center  items-center">
+						<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244079091_1913345055504515_4195074341419206271_n.jpeg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+  			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+
+			<h2 class="text-center text-3xl leading-snug mt-16">El profesionalismo de UMBRELLA SOLUTION'S es de calidad <br> internacional. Trabajan con los mejores materiales y la <br> disposición de su equipo para cualquier proyecto se refleja en <br> los resultados finales. ¡Superan expectativas!</h2>
+			<h2 class="text-center  text-3xl text-black leading-snug">Anthony Almeida, VETERINO</h2>
+	</div>
+
+
+</section>
+
+<section class="w-full h-[50rem] bg-auto bg-white  flex items-end justify-center">
 	<div class="container">
-	<h2 class="text-[89px] text-white font-black leading-none">
-			New </br><u class="text-sky-blue"><span class="text-sky-blue">B</span>e</u>ginnings
+		<div class="flex items-center justify-center">
+			<img class="size-36" src="http://umbrella.local/wp-content/uploads/2024/02/g3924.svg" alt="">
+
+			<h2 class="text-8xl font-black leading-none">
+				<span class="text-sky-blue">P<span class="text-light-gray">remios</span>
+			</h2>
+
+		</div>
+		<h2 class="flex items-center justify-center leading-snug text-6xl">
+			Mejor stand
+		</h2>
+		<h2 class="flex items-center justify-center leading-snug text-6xl font-black">
+			EXPO GANADERA 2022
 		</h2>
 	</div>
 </section>
 
-<!-- Why choosen -->
-<section class="w-full h-[50rem] bg-auto bg-white  flex items-start justify-start ">
+<section class="w-full h-[60rem] bg-auto bg-white  flex items-start justify-center">
 	<div class="container">
 
-		<h2 class="text-[40px] text-sky-blue font-black  text-center"> TESTIMONIALES</h2>
-		<div style="image-rendering: url('');"></div>
-		<div class=" container flex justify-evenly gap-40">
+			<div class="slider  overflow-hidden relative">
+				<div class="slides  transition-transform duration-300">
+					<div class="slide flex  justify-center  items-center">
+						<img class="size-7/12" src="http://umbrella.local/wp-content/uploads/2024/02/image.png" alt="">
+					</div>
+				</div>
+				<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+  				<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+			</div>
 
-		<div class="slider grid grid-cols-3 grid-rows-3 gap-3 overflow-hidden relative">
-
-			<div class="flex flex-col col-span-2 row-span-3 justify-center  items-center">
-				<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244253804_1913345092171178_377135258163533003_n.jpeg" alt="">
-			</div>
-			<div class="flex flex-col justify-center  items-center">
-			<img class ="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244250619_1913344878837866_7039778878385101219_n.jpeg" alt="">
-			</div>
-			<div class="flex flex-col justify-center  items-center">
-				<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244273434_1913344755504545_8294063936522850181_n.jpeg" alt="">
-			</div>
-			<div class="flex flex-col justify-center  items-center">
-				<img class="w-full h-full" src="http://umbrella.local/wp-content/uploads/2024/02/244079091_1913345055504515_4195074341419206271_n.jpeg" alt="">
-			</div>
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2"><</button>
-  			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2">></button>
-		</div>
-	</div>
+			  <h2 class="flex items-center justify-center text-center text-3xl leading-snug mt-8">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed <br> diam nonummy nibh euismod tincidunt ut laoreet dolore <br> magna aliquam erat volutpat. Ut wisi enim ad minim veniam,<br> quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut  </h2>
 	</div>
 </section>
-<section>
-<div class="slider grid grid-cols-3 gap-4 overflow-hidden relative">
-  <div class="slides grid grid-cols-3 transition-transform duration-300">
-    <div class="slide bg-gray-200">Slide 1</div>
-    <div class="slide bg-gray-300">Slide 2</div>
-    <div class="slide bg-gray-400">Slide 3</div>
-  </div>
-  <button class="prev absolute top-1/2 left-0 transform -translate-y-1/2">Anterior</button>
-  <button class="next absolute top-1/2 right-0 transform -translate-y-1/2">Siguiente</button>
-</div>
 </section>
-
 
 <?php
 get_footer();
--- theme/templates/front-services.php
+++ theme/templates/front-services.php
@@ -19,6 +19,154 @@ get_header();
 // Banner
 get_template_part('template-parts/banners/internal', 'page');
 ?>
+<section class="w-full h-[230rem] flex items-center justify-center">
 
+<div class="container">
+	<!-- Publicidad Aérea -->
+	<div class="flex items-center">
+
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/78cf2973-3dde-4f98-be95-98cedfbda7c7.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+		<div>
+		<h2 class=" text-6xl text-sky-blue text-end ">
+		Publicidad <br><span class="text-9xl font-bold">Aér<u>ea</u></span>
+
+		</h2>
+		<h2 class="text-end text-2xl mt-4">
+		Donde todo el mundo te ve.
+		</h2>
+		</div>
+
+
+	</div>
+	<!-- comunicación integral -->
+	<div class="flex items-center">
+
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end ">
+				Comunicación <br><span class="text-9xl font-bold">Integr<u class="mt-4">al</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			La imagen de tu marca <br> habla por sí sola. <br>Branding <br>Packaging <br>Logo (Proceso creativo)
+
+			</h2>
+		</div>
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/disenador-grafico-haciendo-logo-cuaderno-scaled.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+	</div>
+
+	<!-- Publicidad BTL -->
+
+	<div class="flex items-center">
+
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/d70f23f6-4a0e-4ac7-9809-9b7c5a789908.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end">
+			Publicidad <br><span class="text-9xl font-bold">B<u>TL</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			Donde todo el mundo te ve.
+			</h2>
+		</div>
+
+
+	</div>
+	<!-- Diseño y construcción -->
+	<div class="flex items-center">
+
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end">
+			Diseño y construcción de <br><span class="text-9xl font-bold">Stan<u>ds</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			Creatividad y estrategia para posicionamiento de tu marca en ferias, exposiciones, eventos, centros comerciales... ¡Y más!
+
+			</h2>
+		</div>
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="
+		http://umbrella.local/wp-content/uploads/2024/02/aa1093d7-ed7d-4c61-aa04-9a017ae8c880.jpeg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+
+
+	</div>
+	<!-- Rotulación -->
+	<div class="flex items-center">
+
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/65876-scaled.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end">
+			Publicidad <br><span class="text-9xl font-bold">Aér<u>ea</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			Donde todo el mundo te ve.
+			</h2>
+		</div>
+
+
+	</div>
+	<!-- Comunicación digital -->
+	<div class="flex items-center">
+
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end">
+			Comunicación <br><span class="text-9xl font-bold">Integ<u>ral</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			La imagen de tu marca <br> habla por sí sola. <br>Branding <br>Packaging <br>Logo (Proceso creativo)
+
+			</h2>
+		</div>
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/jovenes-usando-carretes-scaled.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+	</div>
+	<!-- Diseño Interior -->
+	<div class="flex items-center">
+
+		<div class="slider overflow-hidden relative">
+			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/cerrar-proyecto-dibujo-arquitecto-scaled.jpg" alt="">
+			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
+			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		</div>
+		<div>
+			<h2 class=" text-6xl text-sky-blue text-end">
+			Publicidad <br><span class="text-9xl font-bold">Aér<u>ea</u></span>
+
+			</h2>
+			<h2 class="text-end text-2xl mt-4">
+			Donde todo el mundo te ve.
+			</h2>
+		</div>
+
+
+	</div>
+
+</div>
+
+</section>
 <?php
 get_footer();
