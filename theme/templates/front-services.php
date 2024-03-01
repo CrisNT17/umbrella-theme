f18bed6f8348a11f5ab6227048b0a4fb218d4d38
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2024-03-01 00:05:10 -0500

    Update portafolio

--- theme/templates/front-services.php
+++ theme/templates/front-services.php
@@ -19,16 +19,24 @@ get_header();
 // Banner
 get_template_part('template-parts/banners/internal', 'page');
 ?>
-<section class="w-full h-[230rem] flex items-center justify-center">
+<section class="w-full h-auto flex items-center justify-center">
 
 <div class="container">
 	<!-- Publicidad Aérea -->
-	<div class="flex items-center">
+	<div class="flex items-center mt-10">
 
-		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/78cf2973-3dde-4f98-be95-98cedfbda7c7.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		<div class="slider overflow-hidden relative h-full">
+			<img class="w-full h-96" src="http://umbrella.local/wp-content/uploads/2024/02/78cf2973-3dde-4f98-be95-98cedfbda7c7.jpg" alt="">
+
+			<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 		<div>
 		<h2 class=" text-6xl text-sky-blue text-end ">
@@ -43,7 +51,7 @@ get_template_part('template-parts/banners/internal', 'page');
 
 	</div>
 	<!-- comunicación integral -->
-	<div class="flex items-center">
+	<div class="flex items-center mt-8">
 
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end ">
@@ -55,21 +63,37 @@ get_template_part('template-parts/banners/internal', 'page');
 
 			</h2>
 		</div>
-		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/disenador-grafico-haciendo-logo-cuaderno-scaled.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		<div class="slider overflow-hidden relative h-full">
+			<img class="size-full" src="http://umbrella.local/wp-content/uploads/2024/02/disenador-grafico-haciendo-logo-cuaderno-scaled.jpg" alt="">
+
+						<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 	</div>
 
 	<!-- Publicidad BTL -->
 
-	<div class="flex items-center">
+	<div class="flex items-center mt-8">
 
 		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/d70f23f6-4a0e-4ac7-9809-9b7c5a789908.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+			<img class="w-full h-96" src="http://umbrella.local/wp-content/uploads/2024/02/d70f23f6-4a0e-4ac7-9809-9b7c5a789908.jpg" alt="">
+
+						<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end">
@@ -84,7 +108,7 @@ get_template_part('template-parts/banners/internal', 'page');
 
 	</div>
 	<!-- Diseño y construcción -->
-	<div class="flex items-center">
+	<div class="flex items-center justify-strech-items mt-8">
 
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end">
@@ -97,68 +121,100 @@ get_template_part('template-parts/banners/internal', 'page');
 			</h2>
 		</div>
 		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="
-		http://umbrella.local/wp-content/uploads/2024/02/aa1093d7-ed7d-4c61-aa04-9a017ae8c880.jpeg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+			<img class="size-full" src="http://umbrella.local/wp-content/uploads/2024/02/aa1093d7-ed7d-4c61-aa04-9a017ae8c880.jpeg" alt="">
+
+						<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 
 
 	</div>
 	<!-- Rotulación -->
-	<div class="flex items-center">
+	<div class="flex items-center justify-between mt-8">
 
-		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/65876-scaled.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		<div class="slider overflow-hidden relative w=4/7">
+			<img class="size-full" src="http://umbrella.local/wp-content/uploads/2024/02/65876-scaled.jpg" alt="">
+
+						<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end">
-			Publicidad <br><span class="text-9xl font-bold">Aér<u>ea</u></span>
+			Rotulación <br><span class="text-9xl font-bold">Publicitar<u>ia</u></span>
 
 			</h2>
 			<h2 class="text-end text-2xl mt-4">
-			Donde todo el mundo te ve.
+			La fuerza de tu marca en <br>exteriores e interiores
+
 			</h2>
 		</div>
 
 
 	</div>
 	<!-- Comunicación digital -->
-	<div class="flex items-center">
+	<div class="flex items-center justify-between mt-8">
 
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end">
-			Comunicación <br><span class="text-9xl font-bold">Integ<u>ral</u></span>
+			Comunicación <br><span class="text-9xl font-bold">Digit<u>al</u></span>
 
 			</h2>
 			<h2 class="text-end text-2xl mt-4">
-			La imagen de tu marca <br> habla por sí sola. <br>Branding <br>Packaging <br>Logo (Proceso creativo)
 
+			Conversación y conexión digital entre tu <br> marca y tus clientes. <br> RRSS • Desarrollo web • CRM <br>Pauta digital (FB, IG, Google, YT
 			</h2>
 		</div>
-		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/jovenes-usando-carretes-scaled.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+
+		<div class="slider overflow-hidden relative w-4/7">
+			<img class="w-full h-96" src="http://umbrella.local/wp-content/uploads/2024/02/jovenes-usando-carretes-scaled.jpg" alt="">
+
+			<!-- Bullets -->
+			<div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
 		</div>
 	</div>
 	<!-- Diseño Interior -->
-	<div class="flex items-center">
+	<div class="flex items-center mt-8">
 
-		<div class="slider overflow-hidden relative">
-			<img class="size-2/3" src="http://umbrella.local/wp-content/uploads/2024/02/cerrar-proyecto-dibujo-arquitecto-scaled.jpg" alt="">
-			<button class="prev absolute top-1/2 left-0 transform -translate-y-1/2 text-4xl"><</button>
-			<button class="next absolute top-1/2 right-0 transform -translate-y-1/2 text-4xl">></button>
+		<div class="slider overflow-hidden relative w-4/7">
+			<img class="size-full" src="http://umbrella.local/wp-content/uploads/2024/02/cerrar-proyecto-dibujo-arquitecto-scaled.jpg" alt="">
+
+						<!-- Bullets -->
+  <div class="absolute bottom-0 right-0 mb-4 mr-4">
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+    	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+  	<span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-3"></span>
+   	 <span class="bg-white rounded-full w-2 h-2 inline-block mr-2"></span>
+  </div>
+			<div class="absolute inset-0  bg-gradient-to-b from-transparent to-black" style="top: 70%; height: 50%; "></div>
 		</div>
 		<div>
 			<h2 class=" text-6xl text-sky-blue text-end">
-			Publicidad <br><span class="text-9xl font-bold">Aér<u>ea</u></span>
+			Diseño <br><span class="text-9xl font-bold">Interi<u>or</u></span>
 
 			</h2>
 			<h2 class="text-end text-2xl mt-4">
-			Donde todo el mundo te ve.
+			Diseño interior para autoservicios, <br> farmacias, locales y presencia en <br>percha. (videos)
 			</h2>
 		</div>
 
