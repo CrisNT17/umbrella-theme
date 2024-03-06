f471bb35f900c4fb3e8d2e162869e5f3e234ae93
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2024-03-06 11:58:18 -0500

    review text services

--- theme/templates/front-services.php
+++ theme/templates/front-services.php
@@ -31,22 +31,23 @@ get_template_part('template-parts/banners/internal', 'page');
 			get_template_part('template-parts/sliders/services', 'right', array('banners' => $sliderServicespa));
 			?>
 		</div>
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
-			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10">
+			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight whitespace-normal max-w-full">
 				Publicidad <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Aérea</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
 			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
 		</div>
 	</div>
 	<!-- Comunicación integral -->
-	<div class="grid md:grid-cols-11 grid-cols-1">
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
-			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
+	<div class="grid md:grid-cols-11 grid-cols-1 ">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10 ">
+			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight ">
 				Comunicación <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Integral</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">
+			La imagen de tu marca<br> habla por sí sola.<br> Branding <Br>Packaging <br>Logo (Proceso creativo).</span>
 		</div>
 		<div class="col-span-6">
 			<?php
@@ -69,22 +70,25 @@ get_template_part('template-parts/banners/internal', 'page');
 			get_template_part('template-parts/sliders/services', 'right', array('banners' => $sliderServicesBTL));
 			?>
 		</div>
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10">
 			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
 				Publicidad <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">BTL</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">
+			Donde tu marca debe estar.<br>
+				Eventos, actividades en sitio <br> (playa, feria de flores, etc.)
+			</span>
 		</div>
 	</div>
 	<!-- Diseño y construcción de stands -->
 	<div class="grid md:grid-cols-11 grid-cols-1">
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10">
 			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
 				Diseño y construcción de <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Stands</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-xl lg:text-lg md:text-lg text-base text-end mt-4">Creatividad y estrategia para<br> posicionamiento de tu marca en ferias,<br> exposiciones, eventos, centros<br> comerciales...<br> ¡Y más!</span>
 		</div>
 		<div class="col-span-6">
 			<?php
@@ -97,7 +101,7 @@ get_template_part('template-parts/banners/internal', 'page');
 		</div>
 	</div>
 	<!-- Rotulación publicitaria -->
-	<div class="grid md:grid-cols-11 grid-cols-1">
+	<div class="grid md:grid-cols-11 grid-cols-1 gap-10 w-full ">
 		<div class="col-span-6">
 			<?php
 			$sliderServicesBTL = array(
@@ -107,22 +111,23 @@ get_template_part('template-parts/banners/internal', 'page');
 			get_template_part('template-parts/sliders/services', 'right', array('banners' => $sliderServicesBTL));
 			?>
 		</div>
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
-			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-full lg:w-full md:w-full w-full font-cocogoose  p-10">
+			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight whitespace-normal max-w-full ">
 				Rotulación <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Publicitaria</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">La fuerza de tu marca en <br> exteriores e interiores
+			</span>
 		</div>
 	</div>
 	<!-- Comunicación digital -->
 	<div class="grid md:grid-cols-11 grid-cols-1">
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10">
 			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
 				Comunicación <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Digital</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Conversación y conexión digital entre tu<br> marca y tus clientes.<br> RRSS • Desarrollo web • CRM <br> Pauta digital (FB, IG, Google, YT</span>
 		</div>
 		<div class="col-span-6">
 			<?php
@@ -145,12 +150,12 @@ get_template_part('template-parts/banners/internal', 'page');
 			get_template_part('template-parts/sliders/services', 'right', array('banners' => $sliderServicesBTL));
 			?>
 		</div>
-		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose">
+		<div class="col-span-5 flex flex-col justify-center items-end xl:w-3/4 lg:w-3/4 md:w-full w-full font-cocogoose p-10">
 			<h3 class="xl:text-5xl lg:text-4xl md:text-3xl text-2xl text-sky-blue text-end font-extralight">
 				Diseño <br><span class="xl:text-7xl lg:text-6xl md:text-5xl text-4xl tracking-widest">Interior</span>
 			</h3>
-			<hr class="bg-sky-blue text-sky-blue rounded-md h-2 md:w-44 w-20 mb-3 font-museo font-light">
-			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Donde todo el mundo te ve.</span>
+			<hr class="bg-sky-blue text-sky-blue  h-2 md:w-44 w-20 mb-3 font-museo font-light">
+			<span class="xl:text-2xl lg:text-lg md:text-lg text-base text-end mt-4">Diseño interior para autoservicios,<br> farmacias, locales y presencia en<br> percha. (videos)</span>
 		</div>
 	</div>
 </section>
